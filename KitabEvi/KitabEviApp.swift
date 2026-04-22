dee33484569e53ce80701a61142712fef0b1f9ba
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2026-04-22 12:10:09 +0300

    Initial Commit

---
+++ KitabEvi/KitabEviApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitabEviApp.swift
+//  KitabEvi
+//
+//  Created by itkhld on 22.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct KitabEviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
